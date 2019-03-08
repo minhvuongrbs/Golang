d70e739475c0f383d85f503fb808c85d950ee1a2
VuongTran <vuong.tran@sioux.asia>
2019-03-08 16:21:43 +0700

    add namespace for api

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	const PORT string = ":8000"
-	log.Printf("Server started at localhost" + PORT + "/wr/")
+	log.Printf("Server started at localhost" + PORT + "/wr/v1")
 	router := _go.NewRouter()
 
 	log.Fatal(http.ListenAndServe(PORT, router))
